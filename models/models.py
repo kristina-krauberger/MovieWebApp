d9753e547031c1d14b94d584a7861c6faba29a20
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-09-25 15:51:13 +0200

    Adds models.

--- models/models.py
+++ models/models.py
@@ -1,3 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class Movies(db.Model):
+    __tablename__ = "movies"
+
+    id = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(100))
+    #user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+
+class FavouriteMovies():
+    __tablename__ = "favourite_movies"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.id"), primary_key=True)
+
+
